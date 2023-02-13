f5efce274fbb8843dca94888f0b87d06b906167a
tiffyy101 <bobatiffy101@gmail.com>
2023-02-13 10:16:29 -0500

    2

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import '/styles/style.css'
 
 const DOM = {
   display: document.getElementById("display"),
@@ -6,16 +6,33 @@ const DOM = {
 }
 const menu = "https://www.themealdb.com/api/json/v1/1/search.php?f=c";
 
-function list() {
-  menu.forEach((meal) => {
-    DOM.mainDiv.insertAdjacentHTML
+async function getData(menu) {
+  try {
+    const response = await fetch(menu);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+
+function createList() {
+  meals.data.forEach((meal) => {
+    DOM.display.insertAdjacentHTML
     ("beforeend",
     `
     <h2>${meal.strMeal}</h2>
     <img class="img" src=${meal.strMealThumb} alt=""/>
+    <button id="choose" class="choose">Tutorial</button>
     `
     )
   });
 }
-menu
-.forEach((meal) => list());
+meals.data.forEach((meal) => createList(meal));
+
+console.log(data);
+    }  
+  } catch (error) {
+    console.log(error);
+    console.log("can't find");
+  }
+}
+getData(menu);
